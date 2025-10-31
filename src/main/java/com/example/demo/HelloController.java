f19d294a971888e3b397ad81828089eda4e5b8d2
Jenkins <jenkins@local>
2025-10-31 18:21:22 +0000

    Build Failure - Rolled back /var/lib/jenkins/workspace/CICDPipeline/src/main/java/com/example/demo/HelloController.java to previous stable commit

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     @GetMapping("/")
-    public String hello() 
-        return "Helloiiii from Java 17 apperem!";
+    public String hello() {
+        return "Helloiiii from Java 17 appere!";
     }
 }
